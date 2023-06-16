1cbda71b06bf6878ceceaff3e15294d8c0963999
nouriyin <courtneyy.noreen@gmail.com>
2023-06-15 23:12:59 -0400

    added

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,5 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 module.exports = mongoose.connection;
